46a6580ba1c9dc894760c1b93f5a2d062e92c93a
Gen Hames <hamesg@tutanota.de>
2020-07-22 12:04:35 -0700

    Deciding...

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-import { css } from 'styletakeout.macro'
+import { css, snip } from 'styletakeout.macro'
 
-const varPink = 'pink';
+// I think snip() makes more sense than snip`...` since TS can enforce the type
+// at write-time instead of waiting for compile-time.
+
+// Uhhhh.... no? Because TS will say snip(funcReturnsString()) is OK - it's not
+const varPink = snip('pink');
+
+// So. snip`pink`. Then throw if there are any expressions at all.
 
 css`
   padding: 5px;
